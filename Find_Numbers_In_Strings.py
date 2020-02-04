cd9705c3b94db2f347e573df04b77d0d7a42e7d3
Nirbhya Kaushik <53752594+Nirbhya@users.noreply.github.com>
2020-02-04 14:42:22 +0530

    Find_Numbers_In_Strings.py
    You have to find numbers in alphanumeric strings.
    input : 12sinjis12ndi3
    output : 12123
    
    input : -132jbf24nif2
    output : -132242

---
+++ Find_Numbers_In_Strings.py
@@ -0,0 +1,25 @@
+#str='-nfn2423fe53gfg'
+str=input('Enter a string, contain numbers : ')
+if(str[0]=='-'):
+        print('-',end="")
+for i in range(0,len(str)):
+    if(str[i]=='0'):
+        print('0',end="")
+    if(str[i]=='1'):
+        print('1',end="")
+    if(str[i]=='2'):
+        print('2',end="")
+    if(str[i]=='3'):
+        print('3',end="")
+    if(str[i]=='4'):
+        print('4',end="")
+    if(str[i]=='5'):
+        print('5',end="")
+    if(str[i]=='6'):
+        print('6',end="")
+    if(str[i]=='7'):
+        print('7',end="")
+    if(str[i]=='8'):
+        print('8',end="")
+    if(str[i]=='9'):
+        print('9',end="")
